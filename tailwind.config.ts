422225e87c389c4f771c9bb97fb8306e3de248bb
naward-cs <nicholas.ward.cyber@gmail.com>
2025-02-18 17:44:13 -0600

    updated tailwindcss for 3xl size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
-import type {Config} from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      screens: {
+        '3xl': '1600px',
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
